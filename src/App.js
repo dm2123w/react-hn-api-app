da4364e9808a33f296d02583f2bdf983992fc983
dmitriysemenko <dm123semenko@gmail.com>
2021-09-03 22:54:17 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,1 +1,10 @@
-export const App = () => <p>Hello</p>;
+import { useState, useEffect } from "react";
+import { getStoryIds } from "./services/hnApi";
+
+export const App = () => {
+    const [storyIds, setStoryIds] = useState([]);
+    useEffect(() => {
+        getStoryIds().then((data) => setStoryIds(data));
+    }, []);
+    return <p>{JSON.stringify(storyIds)}</p>;
+};
---
+++ src/services/hnApi.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const baseURL = "https://hacker-news.firebaseio.com/v0/";
+export const newStoriesURL = `${baseURL}newstories.json`;
+export const storyURL = `${baseURL}item/`;
+
+export const getStoryIds = async () => {
+    const result = await axios.get(newStoriesURL).then(({data}) => data);
+    return result;
+};
